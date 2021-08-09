a79a621af9bad6004a2f65af884e4ad158dc8e05
ringkurahman <mrringkurahman@gmail.com>
2021-08-09 21:58:12 +0400

    Reservation Template, Metabox and Function Created

---
+++ assets/js/reservation.js
@@ -0,0 +1,31 @@
+;(function ($) {
+  $(document).ready(function () {
+    $("#reservenow").on("click", function () {
+      $.post( mealurl.ajaxurl, {
+
+          action: "reservation",
+          name: $("#name").val(),
+          email: $("#email").val(),
+          phone: $("#phone").val(),
+          persons: $("#persons").val(),
+          date: $("#date").val(),
+          time: $("#time").val(),
+          rn: $("#rn").val(),
+        },
+        function (data) {
+          console.log(data)
+          if ("Duplicate" == data) {
+            alert(
+              "You have already placed a request for this reservation. No need to submit again"
+            )
+          } else {
+            $("#paynow").attr("href", data)
+            $("#reservenow").hide()
+            $("#paynow").show()
+          }
+        }
+      )
+      return false
+    })
+  })
+})(jQuery)
--- functions.php
+++ functions.php
@@ -167,3 +167,36 @@ function get_recipe_category( $recipe_id ) {
 
 	return "Food";
 }
+
+
+
+// Reservation Hook
+function meal_process_reservation() {
+
+	if ( check_ajax_referer( 'reservation', 'rn' ) ) {
+		$name    = sanitize_text_field( $_POST['name'] );
+		$email   = sanitize_text_field( $_POST['email'] );
+		$persons = sanitize_text_field( $_POST['persons'] );
+		$phone   = sanitize_text_field( $_POST['phone'] );
+		$date    = sanitize_text_field( $_POST['date'] );
+		$time    = sanitize_text_field( $_POST['time'] );
+
+		$data = array(
+			'name'    => $name,
+			'email'   => $email,
+			'phone'   => $phone,
+			'persons' => $persons,
+			'date'    => $date,
+			'time'    => $time
+		);
+		//print_r( $data );
+
+
+	} else {
+		echo 'Not allowed';
+	}
+	die();
+}
+
+add_action( 'wp_ajax_reservation', 'meal_process_reservation' );
+add_action( 'wp_ajax_nopriv_reservation', 'meal_process_reservation' );
